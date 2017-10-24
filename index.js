a2b8872ff6e9243068c40e22dc188845bb2b2bbc
TJ <cooploops@gmail.com>
2017-10-24 15:44:33 -0700

    add twitter url

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 window.onload = function(){
+    var currentQuote = "";
+    var currentAuthor= "";
     $("#generate").on("click",function(){
         $.ajax({
             type: "GET",
             url: "https://talaikis.com/api/quotes/random/",
         }).done(function(response){
             console.log(response);
+            currentQuote = response.quote;
+            currentAuthor = response.author;
             $("blockquote").empty();
             var quote = $("<p class ='quote'>");
             quote.text(response.quote);
@@ -13,5 +17,7 @@ window.onload = function(){
             $("blockquote").append(quote);
             $("blockquote").append(footer);
         });
-    })
-};
+    });
+
+    $("#tweet-quote").on("click",function() {openURL("href","https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + encodeURIComponent('"'+currentQuote + '" ' + currentAuthor+'"')+'"')
+    });
